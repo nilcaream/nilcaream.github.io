b322c02b02019e002af99f5e04c59d9fe63c0c46
Krzysztof Smigielski <nil.caream@gmail.com>
2019-01-02 21:48:24 +0100

    U cubicles positioning algorithm refactoring; added rounder corners to improve corner cutting.

--- cube/draw.js
+++ cube/draw.js
@@ -2,6 +2,9 @@ var cdraw = {
     begin: function (type) {
         this._type = type;
         this._size = 256;
+        this._space = this._size / 7;
+        this._line = this._size / 5;
+        this._radius = this._size / 12;
         this._origin = this._size * 2;
         this._width = this._size * type + this._origin * 2;
         this._height = this._size * type + this._origin * 2;
@@ -17,6 +20,7 @@ var cdraw = {
         R: "#fe0000",
         D: "#808080",
         black: "#000000",
+        core: "#303030",
     },
     raw: function () {
         return this._canvas.toDataURL("image/png");
@@ -28,17 +32,17 @@ var cdraw = {
         var cropCanvas = this._autoCrop(this._canvas, boundaries);
 
         if (backgroundColor) {
-            var backgroundCanvas = $("<canvas></canvas>").attr("width", cropCanvas.width * (1 + 1 / 64)).attr("height", cropCanvas.height * (1 + 1 / 64))[0];
+            var backgroundCanvas = $("<canvas></canvas>").attr("width", Math.ceil(cropCanvas.width * (1 + 1 / 64))).attr("height", Math.ceil(cropCanvas.height * (1 + 1 / 64)))[0];
             var backgroundContext = backgroundCanvas.getContext("2d");
             backgroundContext.fillStyle = backgroundColor;
             backgroundContext.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-            backgroundContext.drawImage(cropCanvas, cropCanvas.width * 1 / 128, cropCanvas.height * 1 / 128);
+            backgroundContext.drawImage(cropCanvas, Math.ceil(cropCanvas.width * 1 / 128), Math.ceil(cropCanvas.height * 1 / 128));
             cropCanvas = backgroundCanvas;
         }
         if (size) {
             var scale = size / Math.max(width, height);
-            width = Math.ceil(width) * scale;
-            height = Math.ceil(height) * scale;
+            width = Math.ceil(width * scale);
+            height = Math.ceil(height * scale);
             var resizeCanvas = $("<canvas></canvas>").attr("width", width).attr("height", height)[0];
             var resizeContext = resizeCanvas.getContext("2d");
             resizeContext.imageSmoothingQuality = 'high';
@@ -81,11 +85,15 @@ var cdraw = {
         results.height = results.yMax - results.yMin;
         return results;
     },
-    // ["Y11:11,02,012","B"]
-    u: function (colorsArray) {
+    // ["Y11:11,02,012","B","R00:22]
+    u: function (elements) {
         var cdraw = this;
-        colorsArray = colorsArray.slice(0, this._type * this._type);
-        colorsArray.forEach(function (element, index) {
+        // core
+        cdraw._context.fillStyle = cdraw.colorsMap.core;
+        cdraw._context.fillRect(cdraw._origin + cdraw._size / 2, cdraw._origin + cdraw._size / 2, (cdraw._size + cdraw._space) * (cdraw._type - 1), (cdraw._size + cdraw._space) * (cdraw._type - 1));
+
+        elements = elements.slice(0, this._type * this._type);
+        elements.forEach(function (element, index) {
             var mainSplit = element.split(":");
 
             var color = mainSplit[0][0];
@@ -95,23 +103,57 @@ var cdraw = {
 
             var col = index % cdraw._type;
             var row = Math.floor(index / cdraw._type);
+            var size = cdraw._size;
 
-            var x = cdraw._origin + col * (cdraw._size + cdraw._size / 16);
-            var y = cdraw._origin + row * (cdraw._size + cdraw._size / 16);
+            var x = cdraw._origin + col * (cdraw._size + cdraw._space);
+            var y = cdraw._origin + row * (cdraw._size + cdraw._space);
+
+            var rMultiplier = 3;
+            var r = [cdraw._radius, cdraw._radius, cdraw._radius, cdraw._radius];
+
+            // corner cutting improvements
+            if (col === 0 && row === 0) {
+                // corners
+                r[2] *= cdraw._type === 2 ? rMultiplier : 1;
+            } else if (col === cdraw._type - 1 && row === 0) {
+                r[3] *= cdraw._type === 2 ? rMultiplier : 1;
+            } else if (col === 0 && row === cdraw._type - 1) {
+                r[1] *= cdraw._type === 2 ? rMultiplier : 1;
+            } else if (col === cdraw._type - 1 && row === cdraw._type - 1) {
+                r[0] *= cdraw._type === 2 ? rMultiplier : 1;
+                // edges
+            } else if (row === 0) {
+                r[2] *= rMultiplier;
+                r[3] *= rMultiplier;
+            } else if (row === cdraw._type - 1) {
+                r[0] *= rMultiplier;
+                r[1] *= rMultiplier;
+            } else if (col === 0) {
+                r[1] *= rMultiplier;
+                r[2] *= rMultiplier;
+            } else if (col === cdraw._type - 1) {
+                r[0] *= rMultiplier;
+                r[3] *= rMultiplier;
+            } else {
+                // middle
+                r[0] *= rMultiplier;
+                r[1] *= rMultiplier;
+                r[2] *= rMultiplier;
+                r[3] *= rMultiplier;
+            }
+
+            console.log("U" + row + col + " " + color + " o=(" + x + "," + y + ") r=(" + r + ")");
 
             // normal
             cdraw._context.fillStyle = cdraw.colorsMap[color];
-            cdraw._context.fillRect(x, y, cdraw._size, cdraw._size);
-
-            // border
-            cdraw._context.lineWidth = cdraw._size / 8;
             cdraw._context.strokeStyle = cdraw.colorsMap.black;
-            cdraw._roundRect(cdraw._context, x, y, cdraw._size, cdraw._size, cdraw._size / 64);
+            cdraw._context.lineWidth = cdraw._line;
+            cdraw._roundPoly(cdraw._context, [[x, y, r[0]], [x + size, y, r[1]], [x + size, y + size, r[2]], [x, y + size, r[3]]]);
             cdraw._context.stroke();
+            cdraw._context.fill();
 
             // arrows
             cdraw._context.lineWidth = cdraw._size / 16;
-
             cdraw._context.fillStyle = "black";
 
             var midX = x + cdraw._size / 2 + (midDeltaX - 1) * cdraw._size / 4;
@@ -134,11 +176,20 @@ var cdraw = {
     // https://stackoverflow.com/a/7838871
     _roundRect: function (context, x, y, w, h, r) {
         context.beginPath();
-        context.moveTo(x + r, y);
-        context.arcTo(x + w, y, x + w, y + h, r);
-        context.arcTo(x + w, y + h, x, y + h, r);
-        context.arcTo(x, y + h, x, y, r);
-        context.arcTo(x, y, x + w, y, r);
+        context.moveTo(x + r, y); // 0
+        context.arcTo(x + w, y, x + w, y + h, r); // 1-2
+        context.arcTo(x + w, y + h, x, y + h, r); // 2-3
+        context.arcTo(x, y + h, x, y, r); // 3-0
+        context.arcTo(x, y, x + w, y, r); // 0-1
+        context.closePath();
+    },
+    // context, [[1,2,5],[2,3,6],[x,y,r]]
+    _roundPoly: function (context, points) {
+        context.beginPath();
+        context.moveTo(points[0][0] + points[0][2], points[0][1]);
+        for (var i = 0, i1 = 1, i2 = 2; i < points.length; i++, i1 = (i1 + 1) % points.length, i2 = (i2 + 1) % points.length) {
+            context.arcTo(points[i1][0], points[i1][1], points[i2][0], points[i2][1], points[i1][2]);
+        }
         context.closePath();
     },
     // ["Y","B","G"]
