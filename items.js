3bbae710fcba3880b31caaf1e9ab2ec3e29f2748
Krzysztof Smigielski <nil.caream@gmail.com>
2021-01-16 22:40:30 +0100

    Added dynamic lists generation. Implemented data state updates.

--- items.js
+++ items.js
@@ -1,33 +1,72 @@
 'use strict';
 
 $(() => {
-    const data = [];
+    const data = {};
 
     const id = () => new Date().getTime().toString(36) + Math.random().toString(36).substr(-4);
 
-    const draw = (items) => {
-        const target = $(".items").first().empty();
-        items.forEach(item => {
-            const element = $("<li></li>").text(item.content + " " + item.id);
+    const createItem = () => {
+        const item = {
+            id: id(),
+            history: {},
+            order: {}
+        }
+        item.content = item.id;
+        item.history[new Date().getTime().toString()] = "created";
+        return item;
+    };
 
-            target.append(element);
+    const add = (ul) => {
+        const item = createItem();
+        data[item.id] = item;
+        const li = $("<li></li>")
+            .attr("data-item-id", item.id)
+            .text(item.content);
+        ul.append(li);
+        recalculateOrders(ul);
+    };
+
+    const recalculateOrders = (ul) => {
+        const listId = ul.attr("data-list-id");
+        ul.find("li").each((i, element) => {
+            const li = $(element);
+            const itemId = li.attr("data-item-id");
+            data[itemId].order[listId] = i;
         });
     };
 
-    $(".add-item").click(() => {
-        console.log("add item");
-        const item = {
-            "id": id(),
-            "content": "new item",
-            "history": {}
-        }
-        item.history[new Date().getTime().toString()] = "created";
-        data.push(item);
-        draw(data);
+    $(".items-container").each((i, element) => {
+        const container = $(element);
+        const ul = $("<ul></ul>").addClass("items").attr("data-list-id", container.attr("data-list-id"));
+        const addButton = $("<button></button>").text("Add").click(() => {
+            console.log("Add");
+            add(ul);
+        });
+        container.append(ul).append(addButton);
     });
 
     $(".items").sortable({
         connectWith: ".items",
-        dropOnEmpty: true
+        dropOnEmpty: true,
+        remove: (event, ui) => {
+            const ul = $(event.target);
+            const li = ui.item.first();
+            const listId = ul.attr("data-list-id");
+            const itemId = li.attr("data-item-id");
+            delete data[itemId].order[listId];
+            recalculateOrders(ul);
+            console.log(`Removed ${itemId} from ${listId}`);
+            console.log(JSON.stringify(data));
+        },
+        receive: (event, ui) => {
+            const ul = $(event.target);
+            const li = ui.item.first();
+            const listId = ul.attr("data-list-id");
+            const itemId = li.attr("data-item-id");
+            recalculateOrders(ul);
+            console.log(`Added ${itemId} to ${listId}`);
+            console.log(JSON.stringify(data));
+        }
     }).disableSelection();
+
 });
