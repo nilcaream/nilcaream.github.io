2346a4a5c5bb705965da43c8054be4e5f2a2b9cc
Krzysztof Smigielski <nil.caream@gmail.com>
2021-01-18 23:26:45 +0100

    Refactored PoC code. Added local storage persistence.

--- items.js
+++ items.js
@@ -1,103 +1,172 @@
 'use strict';
 
 $(() => {
-    const data = {};
+    const storage = {
+        data: {},
 
-    const id = () => new Date().getTime().toString(36) + Math.random().toString(36).substr(-4);
+        modified: false,
 
-    const createItem = () => {
-        const item = {
-            id: id(),
-            history: {},
-            order: {}
-        }
-        item.content = item.id;
-        item.history[new Date().getTime().toString()] = "created";
-        return item;
-    };
+        startAutoSave: function () {
+            const that = this;
+            setInterval(() => {
+                if (that.modified) {
+                    that.save();
+                }
+            }, 2000);
+        },
 
-    const add = (ul) => {
-        const item = createItem();
-        data[item.id] = item;
-        console.log(`Created item ${item.id}`);
+        load: function () {
+            this.data = JSON.parse(window.localStorage.getItem("nc.items") || "{}");
+            this.modified = false;
+            console.log(`Loaded ${Object.keys(this.data).length} items`);
+        },
 
-        const save = (content) => {
-            content.attr("contenteditable", "false");
-            item.content = content.text().trim();
-        };
+        save: function () {
+            window.localStorage.setItem("nc.items", JSON.stringify(this.data || {}));
+            this.modified = false;
+            console.log(`Saved ${Object.keys(this.data).length} items`);
+        },
 
-        const li = $("<li></li>").attr("data-item-id", item.id);
-        const content = $("<div></div>").addClass("content").text(item.content).focusout(() => save(content));
-        const info = $("<div></div>").addClass("info");
-        const orders = $("<div></div>").addClass("orders");
-        const edit = $("<div></div>").addClass("edit").text("Edit").click(() => {
-            content.toggleClass("editable");
-            if (content.hasClass("editable")) {
-                content.attr("contenteditable", "true");
-                content.focus();
-            } else {
-                save(content);
+        addToHistory: function (item, message) {
+            item.history[new Date().getTime().toString()] = message;
+            this.modified = true;
+        },
+
+        createItem: function () {
+            const item = {
+                id: new Date().getTime().toString(36) + Math.random().toString(36).substr(-4),
+                history: {},
+                order: {},
+                content: "New item"
             }
-        });
-
-        info.append(orders).append(edit);
-        li.append(content).append(info);
-        ul.append(li);
-        recalculateOrders(ul);
-    };
-
-    const recalculateOrders = (ul) => {
-        const listId = ul.attr("data-list-id");
-        ul.find("li").each((i, element) => {
-            const li = $(element);
-            const itemId = li.attr("data-item-id");
-            data[itemId].order[listId] = i;
-
-            const orders = li.find("div.orders").empty();
-            const keys = Object.keys(data[itemId].order);
-            orders.text(keys.join(" | "));
-        });
-        console.log(JSON.stringify(data));
-    };
-
-    $(".items-container").each((i, element) => {
-        const container = $(element);
-        const ul = $("<ul></ul>").addClass("items").attr("data-list-id", container.attr("data-list-id"));
-        const menu = $("<div></div>").addClass("menu");
-        const addMenu = $("<span></span>").text("+").addClass("add").click(() => add(ul));
-        const title = $("<span></span>").text(container.attr("data-list-name")).addClass("title");
-        menu.append(addMenu).append(title);
-        container.append(menu).append(ul);
-    });
-
-    $(".items").sortable({
-        connectWith: ".items",
-        dropOnEmpty: true,
-        remove: (event, ui) => {
-            const ul = $(event.target);
-            const li = ui.item.first();
-            const listId = ul.attr("data-list-id");
-            const itemId = li.attr("data-item-id");
-            delete data[itemId].order[listId];
-            console.log(`Removed ${itemId} from ${listId}`);
-            recalculateOrders(ul);
+            this.addToHistory(item, "created");
+            this.data[item.id] = item;
+            console.log(`Created item ${item.id}`);
+            this.modified = true;
+            return item;
         },
-        receive: (event, ui) => {
-            const ul = $(event.target);
-            const li = ui.item.first();
-            const listId = ul.attr("data-list-id");
-            const itemId = li.attr("data-item-id");
-            console.log(`Added ${itemId} to ${listId}`);
-            recalculateOrders(ul);
+
+        getItems: function (listId) {
+            return Object.values(this.data)
+                .filter(item => item.order[listId])
+                .sort((i0, i1) => i0.order[listId] - i1.order[listId]);
         },
-        stop: (event, ui) => {
-            const ul = $(event.target);
-            const li = ui.item.first();
-            const listId = ul.attr("data-list-id");
-            const itemId = li.attr("data-item-id");
-            console.log(`Stop ${itemId} from ${listId}`);
-            recalculateOrders(ul);
+
+        setOrder: function (itemId, listId, order) {
+            this.data[itemId].order[listId] = order;
+            this.modified = true;
+        },
+
+        removeOrder: function (itemId, listId) {
+            delete this.data[itemId].order[listId];
+            this.modified = true;
+        },
+
+        getListIds: function (itemId) {
+            return Object.keys(this.data[itemId].order);
+        },
+
+        log: function () {
+            Object.values(this.data).forEach((item, i) => console.log(`storage.data[${i}] = ${JSON.stringify(item)}`));
         }
-    });//.disableSelection();
+    };
 
+    const view = {
+        renderItem: function (ul, item) {
+            const setContent = (item, contentDiv) => {
+                contentDiv.attr("contenteditable", "false");
+                item.content = contentDiv.text().trim();
+                storage.modified = true;
+            };
+
+            const li = $("<li></li>").attr("data-item-id", item.id);
+            const contentDiv = $("<div></div>").addClass("content").text(item.content).focusout(() => setContent(item, contentDiv));
+            const infoDiv = $("<div></div>").addClass("info");
+            const ordersDiv = $("<div></div>").addClass("orders");
+            const editDiv = $("<div></div>").addClass("edit").text("Edit").click(() => {
+                contentDiv.toggleClass("editable");
+                if (contentDiv.hasClass("editable")) {
+                    contentDiv.attr("contenteditable", "true");
+                    contentDiv.focus();
+                } else {
+                    setContent(item, contentDiv);
+                }
+            });
+
+            infoDiv.append(ordersDiv).append(editDiv);
+            li.append(contentDiv).append(infoDiv);
+            ul.append(li);
+        },
+
+        createItem: function (ul) {
+            const item = storage.createItem();
+            this.renderItem(ul, item);
+            this.recalculateOrders(ul);
+        },
+
+        recalculateOrders: function (ul) {
+            const listId = ul.attr("data-list-id");
+            ul.find("li").each((i, element) => {
+                const li = $(element);
+                const itemId = li.attr("data-item-id");
+                storage.setOrder(itemId, listId, 100 + i);
+
+                const ordersDiv = li.find("div.orders").empty();
+                ordersDiv.text(storage.getListIds(itemId).join(" | "));
+            });
+            storage.log();
+        },
+
+        initializeContainers: function () {
+            const that = this;
+
+            $(".items-container").each((i, element) => {
+                const container = $(element);
+                const listId = container.attr("data-list-id");
+                const ul = $("<ul></ul>").addClass("items").attr("data-list-id", listId);
+                const menu = $("<div></div>").addClass("menu");
+                const addMenu = $("<span></span>").text("+").addClass("add").click(() => that.createItem(ul));
+                const title = $("<span></span>").text(container.attr("data-list-name")).addClass("title");
+                menu.append(addMenu).append(title);
+                container.append(menu).append(ul);
+
+                storage.getItems(listId).forEach(item => this.renderItem(ul, item));
+                that.recalculateOrders(ul);
+            });
+
+            $(".items").sortable({
+                connectWith: ".items",
+                dropOnEmpty: true,
+                remove: (event, ui) => {
+                    const ul = $(event.target);
+                    const li = ui.item.first();
+                    const listId = ul.attr("data-list-id");
+                    const itemId = li.attr("data-item-id");
+                    storage.removeOrder(itemId, listId);
+                    console.log(`Removed ${itemId} from ${listId}`);
+                    that.recalculateOrders(ul);
+                },
+                receive: (event, ui) => {
+                    const ul = $(event.target);
+                    const li = ui.item.first();
+                    const listId = ul.attr("data-list-id");
+                    const itemId = li.attr("data-item-id");
+                    console.log(`Added ${itemId} to ${listId}`);
+                    that.recalculateOrders(ul);
+                },
+                stop: (event, ui) => {
+                    const ul = $(event.target);
+                    const li = ui.item.first();
+                    const listId = ul.attr("data-list-id");
+                    const itemId = li.attr("data-item-id");
+                    console.log(`Stop ${itemId} from ${listId}`);
+                    that.recalculateOrders(ul);
+                }
+            });//.disableSelection();
+        }
+    };
+
+    storage.load();
+    storage.startAutoSave();
+    view.initializeContainers();
 });
